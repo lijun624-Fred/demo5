b3db0caadd4ebde871092362374f22d9877eb84c
LI Fred (BD/PIP3) <ilf2szh@bosch.com>
2023-02-14 13:07:37 +0800

    ask me

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from datetime import datetime
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
-app = Flask(__name__)
+import requests, json, os
 
+app = Flask(__name__)
+endpoint = "https://demo5.cognitiveservices.azure.cn/language/:query-knowledgebases?projectName=demo5&api-version=2021-10-01&deploymentName=test"
+# data = json.dumps({'question':'how to access azure cn'}) 
+headers = {'Ocp-Apim-Subscription-Key':'d2cae8cd0fd341a39ba457dcd372a7d9', 'Content-Type':'application/json'}
 
 @app.route('/')
 def index():
@@ -19,7 +23,10 @@ def hello():
 
    if name:
        print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
+       data = json.dumps({'question':name}) 
+       req = requests.post(endpoint, data=data, headers=headers)
+       answer = req.json()
+       return render_template('hello.html', name = answer['answers'][0]['answer'])
    else:
        print('Request for hello page received with no name or blank name -- redirecting')
        return redirect(url_for('index'))
